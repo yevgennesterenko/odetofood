e570318b1ebf6948fdd161a552c502044478d6e5
Yevgen Nesterenko <Yevgen_Nesterenko@epam.com>
2015-12-17 16:39:14 +0200

    Initial Commit

---
+++ OdeToFood.Tests/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using NUnit.Framework;
+using OdeToFood.Controllers;
+
+namespace OdeToFood.Tests
+{
+
+    public class HomeControllerTest
+    {
+        [Test]
+        public void About()
+        {
+            HomeController  controller = new HomeController();
+
+            ViewResult result = controller.About() as ViewResult;
+
+            Assert.IsNotNull(result.Model);
+        }
+    }
+}
---
+++ OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OdeToFood.Models;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var model = new AboutModel();
+            model.Name = "Yevgen";
+            model.Location = "Kiev, Ukraine";
+
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
